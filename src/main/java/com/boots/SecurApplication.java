5f16dbc54132a2fd63c9e6472ef03bc3325eaa06
Mikl Klimenko <klimenko.ma.1982@gmail.com>
2023-08-12 18:52:19 +0300

    Added jsp files and moved main class

---
+++ src/main/java/com/boots/SecurApplication.java
@@ -0,0 +1,11 @@
+package com.boots;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SecurApplication.class, args);
+    }
+}
